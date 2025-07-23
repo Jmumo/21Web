4662f037cf75646a06a4bc5e6bfc413f9f348928
John Mumo <john.ngui@equitybank.co.ke>
2025-07-23 15:32:01 +0300

    categories folder

---
+++ 21Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+using _21Web.Data;
+using _21Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _21Web.Controllers;
+
+
+public class CategoryController : Controller
+{
+    private readonly ILogger<CategoryController> _logger;
+    private readonly ApplicationDbContext _context;
+    public CategoryController(ApplicationDbContext context, ILogger<CategoryController> logger)
+    {
+       
+       _context = context;
+       _logger = logger; 
+    }
+    public IActionResult Index()
+    {
+        
+        List<Category>  categories = _context.Categories.ToList();
+        return View(categories);
+    }
+}
---
+++ 21Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,57 @@
+using _21Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _21Web.Data;
+
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+        
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+
+    // protected override  void onModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<Category>().HasData(new Category
+    //     {
+    //         Id = 1,
+    //         Name = "Test",
+    //         Description = "Test",
+    //         DisplayOrder = 1
+    //     });
+    // }
+    
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(new Category
+        {
+            Id = 1,
+            Name = "Action",
+            Description = "Action",
+            DisplayOrder = 1
+        },
+            
+        new Category
+        {
+            Id = 2,
+            Name = "SCIFI",
+            Description = "SCIFI",
+            DisplayOrder = 2
+        },
+        
+        new Category
+            {
+                Id = 3,
+                Name = "History",
+                Description = "History",
+                DisplayOrder = 3
+            }
+        );
+        
+        
+    }
+}
