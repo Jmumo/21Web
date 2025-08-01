227a80e87a8169be795c7c2db145a4cf7ebe6c3f
John Mumo <john.ngui@equitybank.co.ke>
2025-08-01 12:21:26 +0300

    Notification and Update Category functionality

--- 21Web/Controllers/CategoryController.cs
+++ 21Web/Controllers/CategoryController.cs
@@ -26,4 +26,65 @@ public class CategoryController : Controller
     {
         return View();
     }
+    [HttpPost]
+    public IActionResult Create(Category category)
+    {
+
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "Name cannot be the same as Display Order");
+        }
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            TempData["Message"] = "Category Added Successfully";
+            return RedirectToAction("Index", "Category");
+        }
+        return  View();
+    }
+    
+    public IActionResult Edit( int? id )
+    {
+
+        if (id == null || id == 0)
+        {
+            return NotFound(); 
+        }
+        Category category = _context.Categories.Find(id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        return View(category);
+    }
+    
+    [HttpPost]
+    public IActionResult Edit (Category category)
+    {
+
+        if (category.Name == category.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("Name", "Name cannot be the same as Display Order");
+        }
+        if (ModelState.IsValid)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            TempData["Message"] = "Category Updated Successfully";
+            return RedirectToAction("Index", "Category");
+        }
+        return  View();
+    }
+
+    [HttpPost]
+    public IActionResult DeleteCategory(int? id )
+    {
+        
+        Category category = _context.Categories.Find(id);
+        _context.Categories.Remove(category);
+        _context.SaveChanges();
+        TempData["Message"] = "Category Deleted Successfully";
+        return RedirectToAction("Index", "Category");
+    }
 }
--- 21Web/Models/Category.cs
+++ 21Web/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace _21Web.Models;
@@ -7,8 +8,14 @@ public class Category
     [Key]
     public int Id { get; set; }
     [Required]
+    [DisplayName("Category Name")]
+    [StringLength(30, ErrorMessage = "Name must be between 1 and 50 characters")]
     public string Name { get; set; }
     [Required]
+    [DisplayName("Category Description")]
+    [StringLength(200, MinimumLength = 5, ErrorMessage = "Description must be between 5 and 200 characters")]
     public string Description { get; set; }
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100")]
+    [DisplayName("Display Order")]
     public int DisplayOrder { get; set; }
 }
